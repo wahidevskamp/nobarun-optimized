fed9f05e7fcb9bb28cf0edb92916c6ba8728ffa9
Akram Hossain <nayanbd17@gmail.com>
2024-12-31 14:12:34 +0600

    l

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -126,7 +126,7 @@ const IndexPage = ({
                             style={{
                               width: '100%',
                               height: '100%',
-                              minHeight: '110px',
+                              maxHeight: '110px',
                               objectFit: 'contain',
                             }}
                             className="lazyload"
