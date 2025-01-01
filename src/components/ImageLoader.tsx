bb695ba90005392a4b9bd7fd94cee828dbdb792d
Akram Hossain <nayanbd17@gmail.com>
2025-01-01 12:32:50 +0600

    server push

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -1,7 +1,5 @@
-const imageLoader = ({ src, width, height, quality }) => {
-  return `https://d1v2sbji1mlin2.cloudfront.net/${src}?w=${width}&h=${height}&q=${
-    quality || 75
-  }`;
+const imageLoader = ({ src, width = 100, height = 100, quality = 75 }) => {
+  return `https://d1v2sbji1mlin2.cloudfront.net/${src}?w=${width}&h=${height}&q=${quality}`;
 };
 
 export default imageLoader;
