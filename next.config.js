15634b97bf2308c3b64015417586c2e5f531f86e
Akram Hossain <nayanbd17@gmail.com>
2025-01-01 13:09:53 +0600

    Push to server 2

--- next.config.js
+++ next.config.js
@@ -16,40 +16,6 @@ module.exports = {
           }
         ],
       },
-    ];
-  },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // Enable code splitting for modern browsers using the 'module/nomodule' pattern
-      config.output.publicPath = '/_next/';
-
-      // Use babel-loader for modern JavaScript
-      config.module.rules.push({
-        test: /\.(js|mjs|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: 'defaults', // Use a modern browser list
-                  useBuiltIns: 'entry', // Polyfill only necessary features
-                  corejs: 3, // Use core-js 3 for polyfilling
-                },
-              ],
-            ],
-          },
-        },
-      });
-    }
-
-    return config;
-  },
-  // Optional: add custom Babel configuration if necessary (e.g., polyfills and modern JavaScript handling)
-  babel: {
-    presets: ['next/babel', '@babel/preset-env'],
-    plugins: [],
+    ]
   },
 };
