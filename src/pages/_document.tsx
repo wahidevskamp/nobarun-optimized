d883b3055ab002714bd7613f7313c7369e9860bc
Akram Hossain <nayanbd17@gmail.com>
2025-01-02 11:43:33 +0600

    server update 2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,20 +33,18 @@ export default class extends Document {
       <Html lang="en">
         <Head>
           {/* Google Analytics GA4 */}
-          {/* <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-3837KCTGLJ"
-          ></script>
           <script
+            defer={true}
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-3837KCTGLJ');
-              `,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WXL5GDL');
+          `,
             }}
-          /> */}
+          />
           <link
             rel="preload"
             href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600&display=swap"
@@ -64,15 +62,23 @@ export default class extends Document {
               rel="stylesheet"
             />
           </noscript>
-          {/* {process.env.NEXT_PUBLIC_IMAGE_URL && (
+          {process.env.NEXT_PUBLIC_IMAGE_URL && (
             <link
               rel="preconnect"
               href={new URL(process.env.NEXT_PUBLIC_IMAGE_URL).origin}
               crossOrigin="anonymous"
             />
-          )} */}
+          )}
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXL5GDL"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
