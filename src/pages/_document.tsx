ab3158465a914407f1a1e246af90d8b041ffb43b
Akram Hossain <nayanbd17@gmail.com>
2025-01-03 15:54:50 +0600

    google tag manager updated 3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,17 +54,27 @@ export default class extends Document {
           <NextScript />
 
           {/* Google Analytics GA4 */}
+          {/* Lazy Load GA4 */}
           <script
-            id="google-tag-manager"
-            // strategy="afterInteractive"
+            id="ga4-loader"
             dangerouslySetInnerHTML={{
               __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5PKXXJM2');
-          `,
+                document.addEventListener('DOMContentLoaded', function () {
+                  var gaScript = document.createElement('script');
+                  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX';
+                  gaScript.async = true;
+                  document.head.appendChild(gaScript);
+
+                  gaScript.onload = function () {
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag() {
+                      window.dataLayer.push(arguments);
+                    }
+                    gtag('js', new Date());
+                    gtag('config', 'G-XXXXXXX');
+                  };
+                });
+              `,
             }}
           />
         </body>
