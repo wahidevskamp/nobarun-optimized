0eee159b61e80d4170650886abab4e31de2d743a
Akram Hossain <nayanbd17@gmail.com>
2025-01-08 17:42:22 +0600

    server update accroding to doc 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const HomePage = ({ clients, categories, featuredCategories }) => {
                 navigation={false}
                 pagination={false}
                 loop={true}
-                lazy={true}
+                // lazy={true}
               >
                 <SwiperSlide>
                   <img
